a27f48d84ad11e4c27074e981f083122c6838804
vis <97101222+visitf@users.noreply.github.com>
2023-03-28 13:30:55 +0900

    [pandas-36950] update test.sh

--- benchmark_info/pandas/pandas-36950/test.sh
+++ benchmark_info/pandas/pandas-36950/test.sh
@@ -1,1 +1,1 @@
-pytest pandas/tests/frame/apply/test_frame_apply.py::TestDataFrameAggregate::test_agg_args_kwargs
+pytest pandas/tests/frame/apply/test_frame_apply.py::TestDataFrameAggregate::test_agg_args_kwargs_neg
